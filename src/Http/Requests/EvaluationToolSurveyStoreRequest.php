6bb872aff962c494765a999a3f3ef796a4989def
2av - Martin Schmitt <ms@2av.de>
2021-08-12 09:18:59 +0200

    - render validation exception

--- src/Http/Requests/EvaluationToolSurveyStoreRequest.php
+++ src/Http/Requests/EvaluationToolSurveyStoreRequest.php
@@ -24,7 +24,7 @@ class EvaluationToolSurveyStoreRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            "name" => "required"
         ];
     }
 }
--- src/Models/EvaluationToolSurvey.php
+++ src/Models/EvaluationToolSurvey.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class EvaluationToolSurvey extends Model
 {
     use HasFactory;
+
+    protected $fillable = ["name", "description", "published", "publish_up", "publish_down"];
 }
