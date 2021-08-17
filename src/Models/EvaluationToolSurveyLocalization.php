9ff6461a4ac6e1273f4d3e373a0afc5065055cac
Thomas Geissl <tg@2av.de>
2021-08-17 16:31:15 +0200

    fixed localizations crud

--- src/Models/EvaluationToolSurveyLocalization.php
+++ src/Models/EvaluationToolSurveyLocalization.php
@@ -6,8 +6,22 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Twoavy\EvaluationTool\Transformers\EvaluationToolSurveyLocalizationTransformer;
 
 class EvaluationToolSurveyLocalization extends Model
 {
     use HasFactory, SoftDeletes;
+    // transforms data on api responses
+    public $transformer = EvaluationToolSurveyLocalizationTransformer::class;
+
+    // number of items on paginated responses
+    protected $perPage = 25;
+
+    // fields that can be mass-assigned via create or fill methods
+    protected $fillable = [
+        "model",
+        "field",
+        "value",
+        "language_id",
+    ];
 }
---
+++ src/Transformers/EvaluationToolSurveyLocalizationTransformer.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Twoavy\EvaluationTool\Transformers;
+
+use League\Fractal\TransformerAbstract;
+use Twoavy\EvaluationTool\Models\EvaluationToolSurveyLocalization;
+
+class EvaluationToolSurveyLocalizationTransformer extends TransformerAbstract
+{
+    /**
+     * Category transformer.
+     *
+     * @param EvaluationToolSurveyLocalization $surveLocalization
+     * @return array
+     */
+    public function transform(EvaluationToolSurveyLocalization $surveyLocalization): array
+    {
+        return [
+            "id"               => (int)$surveyLocalization->id,
+            "model"             => (string)$surveyLocalization->model,
+            "field"             => (string)$surveyLocalization->field,
+            "value"             => (string)$surveyLocalization->value,
+            "language_id"             => (int)$surveyLocalization->language_id,
+            "createdAt"        => $surveyLocalization->created_at,
+            "updatedAt"        => $surveyLocalization->updated_at,
+            "deletedAt"        => (string)$surveyLocalization->deleted_at,
+        ];
+    }
+
+    public static function originalAttribute($index): ?string
+    {
+        $attributes = self::attributes();
+        return isset($attributes[$index]) ? $attributes[$index] : null;
+    }
+
+    public static function transformedAttribute($index): ?string
+    {
+        $attributes = array_flip(self::attributes());
+        return isset($attributes[$index]) ? $attributes[$index] : null;
+    }
+
+    public static function attributes(): array
+    {
+        return [
+            "id"          => "id",
+            "code"        => "code",
+            "sub_code"    => "sub_code",
+            "title"       => "title",
+            "default"     => "default",
+            "published"     => "published"
+        ];
+    }
+}
