a3e5dce017073b7ed6c4265728da7ca4d621b634
Thomas Geissl <tg@2av.de>
2021-09-01 11:41:07 +0200

    removed seeding

--- tests/Feature/EvaluationToolSurveyTest.php
+++ tests/Feature/EvaluationToolSurveyTest.php
@@ -10,19 +10,14 @@ use Twoavy\EvaluationTool\Models\EvaluationToolSurvey;
 
 class EvaluationToolSurveyTest extends TestCase
 {
-
-    use RefreshDatabase;
-
     public function test_get_surveys()
     {
-        $this->seed(DatabaseSeeder::class);
         $response = $this->get('/api/evaluation-tool/surveys');
         $response->assertStatus(200);
     }
 
     public function test_get_survey()
     {
-        $this->seed(DatabaseSeeder::class);
         $survey   = EvaluationToolSurvey::all()->random(1)[0];
         $response = $this->get('/api/evaluation-tool/surveys/' . $survey->id);
         $response->assertStatus(200);
@@ -30,7 +25,6 @@ class EvaluationToolSurveyTest extends TestCase
 
     public function test_create_survey()
     {
-        $this->seed(DatabaseSeeder::class);
         $data     = [
             "name" => "Test"
         ];
@@ -40,7 +34,6 @@ class EvaluationToolSurveyTest extends TestCase
 
     public function test_create_survey_without_name()
     {
-        $this->seed(DatabaseSeeder::class);
         $data     = [];
         $response = $this->post('/api/evaluation-tool/surveys', $data);
         $response->assertStatus(422);
@@ -48,7 +41,6 @@ class EvaluationToolSurveyTest extends TestCase
 
     public function test_create_survey_with_name_too_short()
     {
-        $this->seed(DatabaseSeeder::class);
         $data     = [
             "name" => "A"
         ];
@@ -59,7 +51,6 @@ class EvaluationToolSurveyTest extends TestCase
     public function test_create_survey_with_name_too_long()
     {
         $faker = Factory::create();
-        $this->seed(DatabaseSeeder::class);
         $data = [
             "name" => $faker->words(100, true)
         ];
@@ -69,7 +60,6 @@ class EvaluationToolSurveyTest extends TestCase
 
     public function test_update_survey()
     {
-        $this->seed(DatabaseSeeder::class);
         $survey   = EvaluationToolSurvey::all()->random(1)[0];
         $data     = $survey->toArray();
         $response = $this->put('/api/evaluation-tool/surveys/' . $survey->id, $data);
@@ -78,7 +68,6 @@ class EvaluationToolSurveyTest extends TestCase
 
     public function test_delete_survey()
     {
-        $this->seed(DatabaseSeeder::class);
         $survey   = EvaluationToolSurvey::all()->random(1)[0];
         $response = $this->delete('/api/evaluation-tool/surveys/' . $survey->id);
         $response->assertStatus(200);
