ceb54cd4fbdde0010496c9685f4999ebd2a48fef
Thomas Geissl <tg@2av.de>
2021-09-01 14:43:26 +0200

    fixed tests

--- tests/Feature/EvaluationToolSurveyTest.php
+++ tests/Feature/EvaluationToolSurveyTest.php
@@ -18,15 +18,15 @@ class EvaluationToolSurveyTest extends TestCase
 
     public function test_get_survey()
     {
-        $survey   = EvaluationToolSurvey::all()->random(1)[0];
+        $survey = EvaluationToolSurvey::all()->random(1)[0];
         $response = $this->get('/api/evaluation-tool/surveys/' . $survey->id);
         $response->assertStatus(200);
     }
 
     public function test_create_survey()
     {
-        $data     = [
-            "name" => "Test"
+        $data = [
+            "name" => "Test",
         ];
         $response = $this->post('/api/evaluation-tool/surveys', $data);
         $response->assertStatus(200);
@@ -34,15 +34,15 @@ class EvaluationToolSurveyTest extends TestCase
 
     public function test_create_survey_without_name()
     {
-        $data     = [];
+        $data = [];
         $response = $this->post('/api/evaluation-tool/surveys', $data);
         $response->assertStatus(422);
     }
 
     public function test_create_survey_with_name_too_short()
     {
-        $data     = [
-            "name" => "A"
+        $data = [
+            "name" => "A",
         ];
         $response = $this->post('/api/evaluation-tool/surveys', $data);
         $response->assertStatus(422);
@@ -50,9 +50,9 @@ class EvaluationToolSurveyTest extends TestCase
 
     public function test_create_survey_with_name_too_long()
     {
-        $faker    = Factory::create();
-        $data     = [
-            "name" => $faker->words(100, true)
+        $faker = Factory::create();
+        $data = [
+            "name" => $faker->words(100, true),
         ];
         $response = $this->post('/api/evaluation-tool/surveys', $data);
         $response->assertStatus(422);
@@ -60,8 +60,8 @@ class EvaluationToolSurveyTest extends TestCase
 
     public function test_update_survey()
     {
-        $survey   = EvaluationToolSurvey::all()->random(1)[0];
-        $data     = $survey->toArray();
+        $survey = EvaluationToolSurvey::all()->random(1)[0];
+        $data = $survey->toArray();
         $response = $this->put('/api/evaluation-tool/surveys/' . $survey->id, $data);
         $response->assertStatus(200);
     }
@@ -69,7 +69,7 @@ class EvaluationToolSurveyTest extends TestCase
     public function test_delete_survey_without_steps()
     {
         EvaluationToolSurveyFactory::times(1)->create();
-        $survey   = EvaluationToolSurvey::all()->last();
+        $survey = EvaluationToolSurvey::all()->last();
         $response = $this->delete('/api/evaluation-tool/surveys/' . $survey->id);
         $response->assertStatus(200);
     }
@@ -77,8 +77,8 @@ class EvaluationToolSurveyTest extends TestCase
     public function test_delete_survey_with_steps()
     {
         $surveyStep = EvaluationToolSurveyStep::all()->last();
-        $survey     = EvaluationToolSurvey::find($surveyStep->survey_id);
-        $response   = $this->delete('/api/evaluation-tool/surveys/' . $survey->id);
+        $survey = EvaluationToolSurvey::find($surveyStep->survey_id);
+        $response = $this->delete('/api/evaluation-tool/surveys/' . $survey->id);
         $response->assertStatus(409);
     }
 }
