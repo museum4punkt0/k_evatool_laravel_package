1aabd50633a2bcf87198728d7e254cbe3dfc031c
2av - Martin Schmitt <ms@2av.de>
2021-09-01 15:30:18 +0200

    - global classname

--- src/Http/Requests/EvaluationToolSurveyElementStoreRequest.php
+++ src/Http/Requests/EvaluationToolSurveyElementStoreRequest.php
@@ -11,14 +11,15 @@ class EvaluationToolSurveyElementStoreRequest extends FormRequest
 {
     public function __construct(Request $request)
     {
-        if($request->has('survey_element_type')){
-            // dd('EvaluationToolSurveyElementType'.ucfirst($request->survey_element_type));
-            $className = 'Twoavy\EvaluationTool\SurveyElementTypes\EvaluationToolSurveyElementType'.ucfirst($request->survey_element_type);
-            if(class_exists($className)){
-                $className::prepareRequest($request);
+        if ($request->has('survey_element_type')) {
+            $this->elementType = ucfirst($request->survey_element_type);
+            $this->className = 'Twoavy\EvaluationTool\SurveyElementTypes\EvaluationToolSurveyElementType' . ucfirst($request->survey_element_type);
+            if (class_exists($this->className)) {
+                $this->className::prepareRequest($request);
             }
         }
     }
+
     /**
      * Determine if the user is authorized to make this request.
      *
@@ -37,12 +38,12 @@ class EvaluationToolSurveyElementStoreRequest extends FormRequest
     public function rules(Request $request): array
     {
         // TODO: rules
-        $rules = [
-            "name"        => "required|min:2|max:100",
+        $rules    = [
+            "name" => "required|min:2|max:100",
             // "description" => "max:500",
             // "published"   => "boolean",
         ];
-        $allRules = array_merge($rules, EvaluationToolSurveyElementTypeMultipleChoice::rules());
-        return $allRules;
+
+        return array_merge($rules, $this->className::rules());
     }
 }
