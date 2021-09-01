0b58e91be5775a2a43099b06bd5c7ecc9e1e4ade
2av - Martin Schmitt <ms@2av.de>
2021-09-01 17:52:34 +0200

    - added survey step store function

--- src/Http/Requests/EvaluationToolSurveyStepStoreRequest.php
+++ src/Http/Requests/EvaluationToolSurveyStepStoreRequest.php
@@ -3,11 +3,18 @@
 namespace Twoavy\EvaluationTool\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Rule;
-use Twoavy\EvaluationTool\Models\EvaluationToolSurvey;
+use Illuminate\Http\Request;
+use Twoavy\EvaluationTool\Helpers\EvaluationToolHelper;
+use Twoavy\EvaluationTool\Transformers\EvaluationToolSurveyStepTransformer;
 
 class EvaluationToolSurveyStepStoreRequest extends FormRequest
 {
+    public function __construct(Request $request)
+    {
+        parent::__construct();
+        EvaluationToolHelper::reverseTransform($request, EvaluationToolSurveyStepTransformer::class);
+    }
+
     /**
      * Determine if the user is authorized to make this request.
      *
@@ -25,6 +32,23 @@ class EvaluationToolSurveyStepStoreRequest extends FormRequest
      */
     public function rules(): array
     {
-        return EvaluationToolSurvey::rules();
+        return [
+            "survey_element_id" => "required|numeric|exists:evaluation_tool_survey_elements,id",
+            "next_step_id"      => "numeric|exists:evaluation_tool_survey_steps,id",
+            "published"         => "boolean",
+            "publish_up"        => [
+                "sometimes",
+                "date",
+                "date_format:Y-m-d H:i:s",
+                "before:publish_down"
+            ],
+            "publish_down"      => [
+                "sometimes",
+                "date",
+                "date_format:Y-m-d H:i:s",
+                "after:publish_up"
+            ],
+            "name"              => "min:2|max:50"
+        ];
     }
 }
