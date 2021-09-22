e49258ae31fb2d1fff57f6706ece14c9b5e71acf
Thomas Geissl <tg@2av.de>
2021-09-22 17:19:47 +0200

    added sample payloads

--- src/Http/Controllers/EvaluationToolSurveySurveyResultController.php
+++ src/Http/Controllers/EvaluationToolSurveySurveyResultController.php
@@ -9,7 +9,6 @@ use Illuminate\Support\Carbon;
 use Ramsey\Uuid\Uuid;
 use Ramsey\Uuid\UuidInterface;
 use StdClass;
-use Twoavy\EvaluationTool\Http\Requests\EvaluationToolSurveyStepStoreRequest;
 use Twoavy\EvaluationTool\Models\EvaluationToolSurvey;
 use Twoavy\EvaluationTool\Models\EvaluationToolSurveyLanguage;
 use Twoavy\EvaluationTool\Models\EvaluationToolSurveyStep;
@@ -22,6 +21,9 @@ class EvaluationToolSurveySurveyResultController extends Controller
     use EvaluationToolResponse;
 
     const STAR_RATING_RESULT_RATING_KEY = 'rating';
+    const BINARY_VALUE_KEY = 'value';
+    const YAYNAY_VALUE_KEY = 'value';
+    const EMOJI_MEANING_KEY = 'meaning';
 
     public function __construct()
     {
@@ -60,7 +62,7 @@ class EvaluationToolSurveySurveyResultController extends Controller
             return $this->errorResponse("no survey step id provided", 409);
         }
 
-        if(!$surveyStep = EvaluationToolSurveyStep::find($request->surveyStepId)){
+        if (!$surveyStep = EvaluationToolSurveyStep::find($request->surveyStepId)) {
             return $this->errorResponse("survey step does not exist", 409);
         }
 
@@ -75,7 +77,7 @@ class EvaluationToolSurveySurveyResultController extends Controller
         $surveyStepResult = new EvaluationToolSurveyStepResult();
         $surveyStepResult->survey_step_id = $request->surveyStepId;
         $surveyStepResult->session_id = $request->uuid;
-        $surveyStepResult->result_value= $request->resultValue;
+        $surveyStepResult->result_value = $request->resultValue;
         $surveyStepResult->result_language_id = $request->languageId;
         $surveyStepResult->params = $surveyStep->survey_element->params;
         $surveyStepResult->answered_at = Carbon::now();
@@ -90,57 +92,65 @@ class EvaluationToolSurveySurveyResultController extends Controller
 
     public function getSampleResultPayload(EvaluationToolSurveyStep $surveyStep): StdClass
     {
-        $payload              = new StdClass;
+        $payload = new StdClass;
         $payload->elementType = $surveyStep->survey_element->survey_element_type->key;
 
-        $samplePayloadFunctionName        = 'samplePayload' . ucfirst($payload->elementType);
-        $payload->resultData              = new StdClass;
+        $samplePayloadFunctionName = 'samplePayload' . ucfirst($payload->elementType);
+        $payload->resultData = new StdClass;
         $payload->resultData->resultValue = $this->{$samplePayloadFunctionName}($surveyStep->survey_element->params);
-        $payload->resultData->languageId  = $this->defaultLanguage->id;
+        $payload->resultData->languageId = $this->defaultLanguage->id;
 
         return $payload;
     }
 
     public function samplePayloadStarRating($params): StdClass
     {
-        $starRatingPayload                                        = new StdClass();
+        $starRatingPayload = new StdClass();
         $starRatingPayload->{self::STAR_RATING_RESULT_RATING_KEY} = 0;
         return $starRatingPayload;
     }
 
     public function samplePayloadMultipleChoice($params)
     {
-        return $params;
+        $multipleChoicePayload = new StdClass();
+        return $multipleChoicePayload;
     }
 
     public function samplePayloadEmoji($params)
     {
-        return $params;
+        $emojiPayload = new StdClass();
+        $emojiPayload->{self::EMOJI_MEANING_KEY} = "";
+        return $emojiPayload;
     }
 
     public function samplePayloadSimpleText($params)
     {
-        return $params;
+        $simpleTextPayload = new StdClass();
+        return $simpleTextPayload;
     }
 
     public function samplePayloadVideo($params)
     {
-        return $params;
+        $videoPayload = new StdClass();
+        return $videoPayload;
     }
 
     public function samplePayloadBinary($params)
     {
-        return $params;
+        $binaryPayload = new StdClass();
+        $binaryPayload->{self::BINARY_VALUE_KEY} = "";
+        return $binaryPayload;
     }
 
     public function samplePayloadYayNay($params)
     {
-        return $params;
+        $yayNayPayload = new StdClass();
+        $yayNayPayload->{self::YAYNAY_VALUE_KEY} = "";
+        return $yayNayPayload;
     }
 
     public function rulesPayloadYayNay($params)
     {
         return $params;
     }
-
 }
