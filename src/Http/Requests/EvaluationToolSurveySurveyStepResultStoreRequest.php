533d7a2cd4539d06cd33446eb7e604a716717dce
Thomas Geissl <tg@2av.de>
2021-09-23 15:24:59 +0200

    trying to debug unkown column error

--- src/Http/Requests/EvaluationToolSurveySurveyStepResultStoreRequest.php
+++ src/Http/Requests/EvaluationToolSurveySurveyStepResultStoreRequest.php
@@ -5,21 +5,24 @@ namespace Twoavy\EvaluationTool\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Request;
 use Twoavy\EvaluationTool\Helpers\EvaluationToolHelper;
-use Twoavy\EvaluationTool\Models\EvaluationToolSurveyStep;
 use Twoavy\EvaluationTool\Models\EvaluationToolSurveyElement;
+use Twoavy\EvaluationTool\Models\EvaluationToolSurveyStep;
+use Twoavy\EvaluationTool\Traits\EvaluationToolResponse;
 use Twoavy\EvaluationTool\Transformers\EvaluationToolSurveyStepResultTransformer;
 
 class EvaluationToolSurveySurveyStepResultStoreRequest extends FormRequest
 {
+    use EvaluationToolResponse;
     public function __construct(Request $request)
     {
         parent::__construct();
 
         EvaluationToolHelper::reverseTransform($request, EvaluationToolSurveyStepResultTransformer::class);
-        $this->surveyStep = EvaluationToolSurveyStep::find($request->survey_step_id);
+        if (!$this->surveyStep = EvaluationToolSurveyStep::find($request->survey_step_id)){
+            return $this->errorResponse("survey step not found", 409);
+        }
         $this->elementType = ucfirst($this->surveyStep->survey_element->survey_element_type->key);
         $this->className = 'Twoavy\EvaluationTool\SurveyElementTypes\EvaluationToolSurveyElementType' . ucfirst($this->surveyStep->survey_element->survey_element_type->key);
-
     }
 
     /**
@@ -42,6 +45,7 @@ class EvaluationToolSurveySurveyStepResultStoreRequest extends FormRequest
 
         // TODO: rules
         $rules = [
+            "survey_step_id" => ['required', 'exists:evaluation_tool_survey_steps']
             // "name"        => "required|min:2|max:100",
             // "description" => "max:500",
             // "published"   => "boolean",
@@ -49,7 +53,7 @@ class EvaluationToolSurveySurveyStepResultStoreRequest extends FormRequest
 
         if (class_exists($this->className)) {
             $element = EvaluationToolSurveyElement::find($this->surveyStep->survey_element_id);
-            return array_merge($rules, $this->className::prepareResultRules($element));
+            array_merge($rules, $this->className::prepareResultRules($element));
         }
         return $rules;
     }
