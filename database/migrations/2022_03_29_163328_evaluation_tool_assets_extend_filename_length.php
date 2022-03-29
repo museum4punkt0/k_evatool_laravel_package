1c650396a7e00229c4b67959f70ce3bf0f1ec5a3
2av - Martin Schmitt <ms@2av.de>
2022-03-29 20:51:57 +0200

    - extended filename field to 100 characters

---
+++ database/migrations/2022_03_29_163328_evaluation_tool_assets_extend_filename_length.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class EvaluationToolAssetsExtendFilenameLength extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('evaluation_tool_assets', function (Blueprint $table) {
+            $table->string('filename', 100)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('evaluation_tool_assets', function (Blueprint $table) {
+            $table->string('filename', 50)->change();
+        });
+    }
+}
